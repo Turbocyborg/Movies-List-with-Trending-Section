604df1dfb5fd8018214fccc10cabab2036dcee2c
Turbocyborg <turbocyborg7@gmail.com>
2025-06-09 17:41:15 +0530

    remove unused auth/context import

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App.jsx';
-import { AuthProvider } from './contexts/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
